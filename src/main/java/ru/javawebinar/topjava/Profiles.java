3e38c04791f209aec775561388505b2add93c6e0
drmaluta <admin@bardsong.com>
2016-10-07 08:06:13 -0700

    HW05

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -9,7 +9,8 @@ public class Profiles {
             POSTGRES = "postgres",
             HSQLDB = "hsqldb",
             JDBC = "jdbc",
-            JPA = "jpa";
+            JPA = "jpa",
+            JPADATA = "jpadata";
 
     public static final String ACTIVE_DB = HSQLDB;
 }
