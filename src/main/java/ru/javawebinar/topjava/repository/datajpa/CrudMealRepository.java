cf4d4b9798e707cb164a994fddbf79d88ab83a4b
drmaluta <admin@bardsong.com>
2016-10-07 00:53:48 -0700

    master

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -0,0 +1,13 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Meal;
+
+/**
+ * gkislin
+ * 02.10.2016
+ */
+@Transactional(readOnly = true)
+public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+}
